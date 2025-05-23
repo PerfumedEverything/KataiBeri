07753c1237642cc3e3f144bdedf03cce7700b04d
PerfumedEverything <shadow1860@mail.ru>
2025-05-23 19:53:07 +0300

    Доработки

--- scripts/Header.js
+++ scripts/Header.js
@@ -1,32 +1,34 @@
 class Header {
-    selectors = {
-        root: '[data-js-header]',
-        overlay: '[data-js-header-overlay]',
-        burgerButton: '[data-js-header-burger-button ]',
-    }
-
-    stateClasses = {
-        isActive: 'is-active',
-        isLock: 'is-lock',
-    }
-
     constructor() {
-        this.rootElement = document.querySelector(this.selectors.root)
-        this.overlayElement = this.rootElement.querySelector(this.selectors.overlay)
-        this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton)
-        this.bindEvents()
+      this.header = document.querySelector('[data-js-header]');
+      this.burger = document.querySelector('[data-js-header-burger-button]');
+      this.nav = document.querySelector('[data-js-header-overlay]');
+      this.navLinks = document.querySelectorAll('[data-js-menu-item]');
+      this.init();
     }
-
-    onBurgerButtonClick = () => {
-        this.burgerButtonElement.classList.toggle(this.stateClasses.isActive)
-        this.overlayElement.classList.toggle(this.stateClasses.isActive)
-        document.documentElement.classList.toggle(this.stateClasses.isLock)
+  
+    init() {
+      if (this.header) {
+        this.burger.addEventListener('click', () => {
+          this.nav.classList.toggle('is-active');
+          this.burger.classList.toggle('is-active');
+        });
+  
+        window.addEventListener('click', e => {
+          if (!e.target.closest('[data-js-header-overlay]') && !e.target.closest('[data-js-header-burger-button]')) {
+            this.nav.classList.remove('is-active');
+            this.burger.classList.remove('is-active');
+          }
+        });
+  
+        this.navLinks.forEach(link => {
+          link.addEventListener('click', () => {
+            this.nav.classList.remove('is-active');
+            this.burger.classList.remove('is-active');
+          });
+        });
+      }
     }
-
-    bindEvents() {
-        this.burgerButtonElement.addEventListener('click', this.onBurgerButtonClick)
-    }
-
-}
-
-export default Header
+  }
+  
+  export default Header;
